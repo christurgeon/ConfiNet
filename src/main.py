c641298efa58f480a1ce77a76639d18560aac2a1
christurgeon <turgeonchris3@outlook.com>
2019-09-14 23:40:49 -0400

    Port scanning and meta data functional

--- src/main.py
+++ src/main.py
@@ -1,4 +1,18 @@
 import threading # use this to concurrently search for open ports
+import datetime
+import os
+from port_scanner import search
+
+
+class TextColors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
 def displayASCII():
@@ -8,12 +22,32 @@ def displayASCII():
     print("| |    / _ \| '_ \|  _| | '_ \ / _ \ __|")
     print("| |___| (_) | | | | | | | | | |  __/ |_ ")
     print(" \_____\___/|_| |_|_| |_|_| |_|\___|\__|\n")     
+    print("--help for menu")
+    print("--quit to exit\n")
 
 
-if __name__ == "__main__":
+def getMetaData(file, outfile=None):
+    output = ""
+    try:
+        stats = os.stat(file)
+        output += "File Size: " + str(stats.st_size) + " bytes\n"
+        output += "File Modified: " + str(datetime.datetime.fromtimestamp(stats.st_mtime / 1000.0)) + "\n"
+        output += "I-node: " + str(stats.st_ino) + "\n"
+        output += "Device: " + str(stats.st_dev) + "\n"
+        output += "Hardlinks: " + str(stats.st_nlink) + "\n"
+        output += "User Id (uid): " + str(stats.st_uid) + "\n"
+        output += "Group Id (gid): " + str(stats.st_gid) + "\n"
+        output += "NOTE: On a Windows systems these values may not be accurate\n"
+        if outfile:
+            output.write(output)
+        else:
+            print(output)
+    except:
+        print("ERROR: Failed to get information from", file)
 
-    displayASCII()
-    print("\n---- Command List ----\n")
+
+def printHelpMenu():
+    print(TextColors.GREEN + "\n---- Command List ----\n" + TextColors.END)
 
     print("scan")
     print("\n\t- Displays all open ports within the specified range on the provided IP address")
@@ -30,11 +64,41 @@ if __name__ == "__main__":
     print("\t-   Usage: whois IP_ADDRESS")
     print("\t- Example: whois 127.0.0.1\n")
 
-    print("---- Optional Flags ----")
+    print("meta")
+    print("\n\t- Displays meta data about a file")
+    print("\t-   Usage: meta SOME_FILE")
+    print("\t- Example: meta ./src/file.txt\n")
+
+    print(TextColors.GREEN + "---- Optional Flags ----" + TextColors.END)
     print("\n-d")
     print("\n\t- Dumps the contents to a file")
-    print("\t- Usage: -d file.txt")
-    
+    print("\t- Usage: -d file.txt\n")
 
-    # -f means all that comes after it is a file path
-    # Can enter text in a string "hello there"
+
+if __name__ == "__main__":
+
+    displayASCII()
+    while True:
+
+        command = str(input(TextColors.BLUE + "command$ " + TextColors.END))
+        if command == "--help":
+            printHelpMenu()
+            continue
+        if command == "--quit":
+            print("\nGoodbye!")
+            break
+
+        tokens = command.split(" ")
+        print(tokens, end="\n")
+
+        if command.startswith("scan"):
+            if len(tokens) == 4:
+                search(tokens[1], tokens[2], tokens[3])
+        elif command.startswith("locate"):
+            continue
+        elif command.startswith("whois"):
+            continue
+        elif command.startswith("meta"):
+            getMetaData(tokens[1], None)
+        else:
+            print("ERROR: Command not recognized")
--- src/port_scanner.py
+++ src/port_scanner.py
@@ -1,11 +1,23 @@
 import socket
-import threading
 
 MAXPORT = 65535
 
-def searchPorts(ip_addr, start_port, end_port):
-    
+
+"""
+add multithreading to the port search to make it much quicker
+...
+...
+might need mutex locks for the add process
+"""
+
+def search(ip_addr, start_port, end_port):
+
     # Validate input 
+    try:
+        start_port = int(start_port)
+        end_port = int(end_port)
+    except:
+        print("ERROR: Invalid port numbers entered")
     if start_port < 0 or end_port < 0:
         return -1
     if start_port > MAXPORT or end_port > MAXPORT:
@@ -18,7 +30,7 @@ def searchPorts(ip_addr, start_port, end_port):
     # Check for open ports and add them to a list
     open_ports = []
     for port in range(start_port, end_port + 1):
-        sock = socket.socket(socket.AF_NET, socket.SOCK_STREAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex( (ip_addr, port) )
         if result == 0:
             open_ports.append(port)
